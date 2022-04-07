d0325e7d91826f2d144ac1a7ce65c2864cfc172f
lucass0717 <lucasharbani@gmail.com>
2022-04-07 02:21:55 -0400

    beginning of stacked bar chart

--- js/visualization.js
+++ js/visualization.js
@@ -3,26 +3,8 @@ const margin = { top: 50, right: 50, bottom: 50, left: 200 };
 const width = 1250 - margin.left - margin.right;
 const height = 650 - margin.top - margin.bottom;
 
-/*
-Line chart (chart 1) set up and initialization
-*/ 
-// Append svg object to the body of the page to house linechart1
-const svg1 = d3.select("#vis1-container")
-                .append("svg")
-                .attr("class", "charts")
-                .attr("width", width - margin.left - margin.right)
-                .attr("height", height - margin.top - margin.bottom)
-                .attr("viewBox", [0, 0, width, height]); 
-
 // Initialize brush for linechart1 and points. So that they are global. 
 let myLine1; 
-
-const svg2 = d3.select("#vis4-container")
-                .append("svg")
-                .attr("class", "charts")
-                .attr("width", width - margin.left - margin.right)
-                .attr("height", height - margin.top - margin.bottom)
-                .attr("viewBox", [0, 0, width, height]); 
 
 // Define color scale
 // const color = d3.scaleOrdinal()
@@ -33,9 +15,6 @@ const svg2 = d3.select("#vis4-container")
 d3.csv("data/composite_wordle_data.csv").then((data) => {
   
   console.table(data);
-
-  // console.log("hi");
-  console.log(data[0].date);
   
   // So that the scales for all of the following charts are global
   let x1, y1, x2, y2, x3, y3;  
@@ -47,6 +26,14 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
   Scatterplot1
   */
   {
+    // Append svg object to the body of the page to house linechart1
+    const svg = d3.select("#vis1-container")
+      .append("svg")
+      .attr("class", "charts")
+      .attr("width", width - margin.left - margin.right)
+      .attr("height", height - margin.top - margin.bottom)
+      .attr("viewBox", [0, 0, width, height]); 
+
     // initializing the x and y axes keys
     xKey1 = "date";
     yKey1 = "number_of_players";
@@ -62,7 +49,7 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
             .range([margin.left, width-margin.right]);
         
     // Add x axis 
-    svg1.append("g")
+    svg.append("g")
         .attr("transform", `translate(0,${height - margin.bottom})`) 
         .call(d3.axisBottom(x1)   
           .tickFormat(d3.timeFormat("%m/%d")))
@@ -90,7 +77,7 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
                 .range([height - margin.bottom, margin.top]);
 
     // Add y axis 
-    svg1.append("g")
+    svg.append("g")
         .attr("transform", `translate(${margin.left}, 0)`) 
         .call(d3.axisLeft(y1)) 
         .attr("font-size", '20px') 
@@ -128,7 +115,7 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
     };
 
     // Add points
-    myLine1 = svg1.selectAll("circle")
+    myLine1 = svg.selectAll("circle")
                             .data(data)
                             .enter()
                               .append("circle")
@@ -146,7 +133,7 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
     .y((d) => y1(d[yKey1])) 
     .curve(d3.curveMonotoneX)
     
-    svg1.append("path")
+    svg.append("path")
     .datum(data) 
     .attr("class", "line") 
     .attr("d", line)
@@ -155,124 +142,133 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
     .style("stroke-width", "2");
   }
 
+
   /*
   BarChart
   */
   {
-  // initializing the x and y axes keys
-  xKey2 = "date";
-  yKey2 = "performance";
+    // attach a new svg canvas to the respective div
+    const svg = d3.select("#vis4-container")
+                .append("svg")
+                .attr("class", "charts")
+                .attr("width", width - margin.left - margin.right)
+                .attr("height", height - margin.top - margin.bottom)
+                .attr("viewBox", [0, 0, width, height]); 
 
-  // Find max x
-  var parseTime = d3.timeParse("%m/%d/%Y");
-  let maxX2 = d3.max(data, (d) => { return parseTime(d[xKey2]); });
-  let minX2 = d3.min(data, (d) => { return parseTime(d[xKey2]); });
+    // initializing the x and y axes keys
+    xKey2 = "date";
+    yKey2 = "performance";
 
-  // Create X scale
-  let xScale2 = d3.scaleLinear()
-          .domain([minX2,maxX2])
-          .range([margin.left, width-margin.right]);
-      
-  // Add x axis 
-  svg2.append("g")
-      .attr("transform", `translate(0,${height - margin.bottom})`) 
-      .call(d3.axisBottom(xScale2)   
-        .tickFormat(d3.timeFormat("%m/%d")))
-      .attr("font-size", '20px')
-      .call((g) => g.append("text")
-                    .attr("x", width - margin.right)
-                    .attr("y", margin.bottom - 4)
-                    .attr("fill", "black")
-                    .attr("text-anchor", "end")
-                    .text(xKey2));
+    // Find max x
+    var parseTime = d3.timeParse("%m/%d/%Y");
+    let maxX2 = d3.max(data, (d) => { return parseTime(d[xKey2]); });
+    let minX2 = d3.min(data, (d) => { return parseTime(d[xKey2]); });
 
-  // Finx max y 
-  // var maxY1 = d3.max(data, (d) => { return d.number_of_players; });
-  // var minY1 = d3.min(data, function(d){ return d.number_of_players; });
-  // ext = d3.extent(data, (d) => {return d[yKey1]});
-  // console.log("key1:" + ext);
-  // console.log(maxY1);
-  // console.log(minY1);
-  maxY2 = 8.5;
-  minY2 = 4.0;
+    // Create X scale
+    let xScale2 = d3.scaleLinear()
+            .domain([minX2,maxX2])
+            .range([margin.left, width-margin.right]);
+        
+    // Add x axis 
+    svg.append("g")
+        .attr("transform", `translate(0,${height - margin.bottom})`) 
+        .call(d3.axisBottom(xScale2)   
+          .tickFormat(d3.timeFormat("%m/%d")))
+        .attr("font-size", '20px')
+        .call((g) => g.append("text")
+                      .attr("x", width - margin.right)
+                      .attr("y", margin.bottom - 4)
+                      .attr("fill", "black")
+                      .attr("text-anchor", "end")
+                      .text(xKey2));
 
-  // Create Y scale
-  let yScale2 = d3.scaleLinear()
-              .domain([minY2, maxY2])
-              .range([height - margin.bottom, margin.top]);
+    // Finx max y 
+    // var maxY1 = d3.max(data, (d) => { return d.number_of_players; });
+    // var minY1 = d3.min(data, function(d){ return d.number_of_players; });
+    // ext = d3.extent(data, (d) => {return d[yKey1]});
+    // console.log("key1:" + ext);
+    // console.log(maxY1);
+    // console.log(minY1);
+    maxY2 = 8.5;
+    minY2 = 4.0;
 
-  // Add y axis 
-  svg2.append("g")
-      .attr("transform", `translate(${margin.left}, 0)`) 
-      .call(d3.axisLeft(yScale2)) 
-      .attr("font-size", '20px') 
-      .call((g) => g.append("text")
-                    .attr("x", 0)
-                    .attr("y", margin.top - 20)
-                    .attr("fill", "black")
-                    .attr("text-anchor", "end")
-                    .text("Performance"));
+    // Create Y scale
+    let yScale2 = d3.scaleLinear()
+                .domain([minY2, maxY2])
+                .range([height - margin.bottom, margin.top]);
 
-  const yTooltipOffset = 15; 
+    // Add y axis 
+    svg.append("g")
+        .attr("transform", `translate(${margin.left}, 0)`) 
+        .call(d3.axisLeft(yScale2)) 
+        .attr("font-size", '20px') 
+        .call((g) => g.append("text")
+                      .attr("x", 0)
+                      .attr("y", margin.top - 20)
+                      .attr("fill", "black")
+                      .attr("text-anchor", "end")
+                      .text("Performance"));
 
-  // Adds a tooltip with the information
-  let tooltip = d3.select("#vis4-container") 
-                  .append("div3") 
-                  .attr('id', "tooltip3") 
-                  .style("opacity", 0) 
-                  .attr("class", "tooltip");
+    const yTooltipOffset = 15; 
 
-  // Mouseover event handler
-  let mouseover = function(event, d) {
-  tooltip.html("Date: " + d[xKey2] + "<br> Performance: " + d[yKey2] + "<br>")
-          .style("opacity", 1);
-  };
+    // Adds a tooltip with the information
+    let tooltip = d3.select("#vis4-container") 
+                    .append("div3") 
+                    .attr('id', "tooltip3") 
+                    .style("opacity", 0) 
+                    .attr("class", "tooltip");
 
-  // Mouse moving event handler
-  let mousemove = function(event) {
-  tooltip.style("left", (event.pageX)+"px") 
-          .style("top", (event.pageY + yTooltipOffset) +"px");
-  };
+    // Mouseover event handler
+    let mouseover = function(event, d) {
+    tooltip.html("Date: " + d[xKey2] + "<br> Performance: " + d[yKey2] + "<br>")
+            .style("opacity", 1);
+    };
 
-  // Mouseout event handler
-  let mouseleave = function() { 
-  tooltip.style("opacity", 0);
-  };
+    // Mouse moving event handler
+    let mousemove = function(event) {
+    tooltip.style("left", (event.pageX)+"px") 
+            .style("top", (event.pageY + yTooltipOffset) +"px");
+    };
 
-  // Create x scale
-  let barWidth = d3.scaleBand()
-            .domain(d3.range(data.length))
-            .range([margin.left, width - margin.right])
-            .padding(0.1); 
+    // Mouseout event handler
+    let mouseleave = function() { 
+    tooltip.style("opacity", 0);
+    };
 
-  // Add points
-  let myLine2 = svg2.selectAll(".bar")
-                          .data(data)
-                          .enter()
-                            .append("rect")
-                            .attr("class", "bar")
-                            .attr("id", (d) => d.wordle_id)
-                            .attr("x", (d,i) => x1(parseTime(d[xKey1])))
-                            .attr("y", (d) => yScale2(d[yKey2]))
-                            .attr("width", 15)
-                            .attr("height", (d) => (height - margin.bottom) - yScale2(d[yKey2]))
-                            .style("opacity", 1)
-                            .style("fill", function(d){ 
-                              if (d[yKey2] <= 4.413630118) {
-                                return 'black' 
-                              } else if (d[yKey2] >= 4.432132964 && d[yKey2] < 5.510370517) {
-                                return '#787c7e'
-                              } else if (d[yKey2] >= 5.53633218 && d[yKey2] < 6.218866798) {
-                                return '#cab558'
-                              } else {
-                                return '#6aaa64'
-                              }})
-                            .on("mouseover", mouseover) 
-                            .on("mousemove", mousemove)
-                            .on("mouseleave", mouseleave);
+    // Create x scale
+    let barWidth = d3.scaleBand()
+              .domain(d3.range(data.length))
+              .range([margin.left, width - margin.right])
+              .padding(0.1); 
+
+    // Add points
+    let myLine2 = svg.selectAll(".bar")
+                            .data(data)
+                            .enter()
+                              .append("rect")
+                              .attr("class", "bar")
+                              .attr("id", (d) => d.wordle_id)
+                              .attr("x", (d,i) => x1(parseTime(d[xKey1])))
+                              .attr("y", (d) => yScale2(d[yKey2]))
+                              .attr("width", 15)
+                              .attr("height", (d) => (height - margin.bottom) - yScale2(d[yKey2]))
+                              .style("opacity", 1)
+                              .style("fill", function(d){ 
+                                if (d[yKey2] <= 4.413630118) {
+                                  return 'black' 
+                                } else if (d[yKey2] >= 4.432132964 && d[yKey2] < 5.510370517) {
+                                  return '#787c7e'
+                                } else if (d[yKey2] >= 5.53633218 && d[yKey2] < 6.218866798) {
+                                  return '#cab558'
+                                } else {
+                                  return '#6aaa64'
+                                }})
+                              .on("mouseover", mouseover) 
+                              .on("mousemove", mousemove)
+                              .on("mouseleave", mouseleave);
   }
 
-  /* Word Cloud */
+  /* WORD CLOUD */
   {
 
     let words = []
@@ -285,8 +281,8 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
 
     // Dimensions and Margins of the graph
     let margin = {top: 10, right: 10, bottom: 10, left: 10};
-    let width = 450 - margin.left - margin.right;
-    let height = 450 - margin.top - margin.bottom;
+    let width = 600 - margin.left - margin.right;
+    let height = 600 - margin.top - margin.bottom;
     
     // append the svg object to the page
     let svg = d3.select("#vis3-container")
@@ -322,5 +318,24 @@ d3.csv("data/composite_wordle_data.csv").then((data) => {
         })
         .text(function(d) {return d.text});
     }
+  }
+
+  /* STACKED BAR CHART */
+  {
+    // Dimensions and Margins of the graph
+    let margin = {top: 10, right: 10, bottom: 10, left: 10};
+    let width = 600 - margin.left - margin.right;
+    let height = 600 - margin.top - margin.bottom;
+    
+    // append the svg canvas to the page
+    let svg = d3.select("#vis5-container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+              `translate(${margin.left},${margin.top})`);
+    
+    
   }
 });
