89a2a89e3506d146efaf2f395eb8de427cd6dd96
lucass0717 <lucasharbani@gmail.com>
2022-04-13 11:43:36 -0400

    layout and format fixes

--- js/word_cloud.js
+++ js/word_cloud.js
@@ -1,105 +1,102 @@
 let drawWordCloud = function(rarity, word_cloud_svg) {
 
-let activeWord;
-d3.csv("data/composite_wordle_data.csv").then((data) => {
-    {
-        const height = 800 - margin.top - margin.bottom;
+  let activeWord;
+  d3.csv("data/composite_wordle_data.csv").then((data) => {
+    const height = 800 - margin.top - margin.bottom;
 
-        // Create an array of sizes based off number of tries
-        num_tries = data.map(function(d) {return d.avg_num_of_tries});
-        let min_tries = d3.min(num_tries);
-        let max_tries = d3.max(num_tries);
-        let tries_size_scale = d3.scaleSqrt()
-          .domain([min_tries, max_tries])
-          .range([15, 100]);
-        tries_sizes = num_tries.map(function(d) {
-          return Math.ceil(tries_size_scale(d) / 10) * 10;
-        });
-    
-        // Create an array of JSON for the word and the size
-        // according to rarity or performance
-        let words = [];
-        for(let i = 0; i < data.length; i++) {
-            let current_size = rarity ? data[i].rarity * 20 : tries_sizes[i];
-            words.push({
-              word: data[i].word,
-              size: current_size});
-        }
+    // Create an array of sizes based off number of tries
+    num_tries = data.map(function(d) {return d.avg_num_of_tries});
+    let min_tries = d3.min(num_tries);
+    let max_tries = d3.max(num_tries);
+    let tries_size_scale = d3.scaleSqrt()
+      .domain([min_tries, max_tries])
+      .range([15, 90]);
+    tries_sizes = num_tries.map(function(d) {
+      return Math.ceil(tries_size_scale(d) / 10) * 10;
+    });
 
-        const yTooltipOffset = 15; 
+    // Create an array of JSON for the word and the size
+    // according to rarity or performance
+    let words = [];
+    for(let i = 0; i < data.length; i++) {
+      let current_size = rarity ? data[i].rarity * 20 : tries_sizes[i];
+      words.push({
+        word: data[i].word,
+        size: current_size});
+    }
 
-        // Adds a tooltip with the information
-        let tooltip = d3.select("#word-cloud") 
-                        .append("div3") 
-                        .attr('id', "tooltip3") 
-                        .style("opacity", 0) 
-                        .attr("class", "tooltip");
+    const yTooltipOffset = 15; 
 
-        // Mouseover event handler
-        let mouseover = function(event, d) {
-          activeWord = d.text
-          tooltip.html("Word: " + activeWord)
-                  .style("opacity", 1);
-          d3.selectAll(".bar_"+activeWord)
-          .transition().style('outline','solid')
-        };
+    // Adds a tooltip with the information
+    let tooltip = d3.select("#word-cloud") 
+                    .append("div3") 
+                    .attr('id', "tooltip3") 
+                    .style("opacity", 0) 
+                    .attr("class", "tooltip");
 
-        // Mouse moving event handler
-        let mousemove = function(event) {
-        tooltip.style("left", (event.pageX)+"px") 
-                .style("top", (event.pageY + yTooltipOffset) +"px");
-        };
+    // Mouseover event handler
+    let mouseover = function(event, d) {
+      activeWord = d.text
+      tooltip.html("Word: " + activeWord)
+              .style("opacity", 1);
+      d3.selectAll(".bar_"+activeWord)
+      .transition().style('outline','solid')
+    };
 
-        // Mouseout event handler
-        let mouseleave = function() { 
-        tooltip.style("opacity", 0);
-        d3.selectAll(".bar_"+activeWord)
-        .transition().style('outline','none')
-        };
+    // Mouse moving event handler
+    let mousemove = function(event) {
+    tooltip.style("left", (event.pageX)+"px") 
+            .style("top", (event.pageY + yTooltipOffset) +"px");
+    };
 
-    
-        console.log(`Width: ${width - margin.left - margin.right}`);
-        console.log(`Height: ${height}`)
+    // Mouseout event handler
+    let mouseleave = function() { 
+    tooltip.style("opacity", 0);
+    d3.selectAll(".bar_"+activeWord)
+    .transition().style('outline','none')
+    };
 
-    
-        let layout = d3.layout.cloud()
-            .size([width,height])
-            .words(words.map(function(d) { return {text: d.word, size:d.size}; }))
-            .padding(10)
-            .rotate(function() { return 0;})
-            .fontSize(function(d) { return d.size; })
-            .on("end", draw);
-    
-        layout.start();
-    
-        function draw(words) {
-          word_cloud_svg.append("g")
-            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .attr("class", (d) => "word_" + d.text)
-            .style("font-size", (d) => d.size)
-            .style("fill", function(d,i){ 
-              if (data[i].rarity == 1) {
-                return '#000000'; 
-              } else if (data[i].rarity == 2) {
-                return '#cab558';
-              } else {
-                return '#6aaa64';
-              }
-            })
-            .attr("text-anchor", "middle")
-            .style("font-family", "Open Sans")
-            .attr("transform", function(d) {
-              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) {return d.text})
-            .on("mouseover", mouseover) 
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
-        }
-      }
-});
 
+    console.log(`Width: ${width - margin.left - margin.right}`);
+    console.log(`Height: ${height}`)
+
+
+    let layout = d3.layout.cloud()
+      .size([width,height])
+      .words(words.map(function(d) { return {text: d.word, size:d.size}; }))
+      .padding(10)
+      .rotate(function() { return 0;})
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw);
+
+    layout.start();
+
+    function draw(words) {
+      word_cloud_svg.append("g")
+        .attr("transform", "translate(" + (layout.size()[0] / 2) + "," + layout.size()[1] / 2 + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .attr("class", (d) => "word_" + d.text)
+        .style("font-size", (d) => d.size)
+        .style("fill", function(d,i){ 
+          if (data[i].rarity == 1) {
+            return '#000000'; 
+          } else if (data[i].rarity == 2) {
+            return '#cab558';
+          } else {
+            return '#6aaa64';
+          }
+        })
+        .attr("text-anchor", "middle")
+        .style("font-family", "Open Sans")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) {return d.text})
+        .on("mouseover", mouseover) 
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+    }
+  });
 };
